42405dc33203050702dff28eec8e21eed561e67c
Eleanor Q <elquandt@gmail.com>
2022-04-23 22:37:13 -0700

    add first funciton for first test

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+
+
+function beepBoop(num) {
+    const numInput = parseInt(num);
+    const emptyArray = [];
+    for (let index = 1; index <= num; index++);
+}
+return emptyArray("123");
+
+
+
+
+console.log(index);
+
+
+const emptyArray = [];
+const answerArray = array.map(function(num) {
+    return num
+});
+answerArray;
+    return all numbers from 1 to the number they chose
+}
